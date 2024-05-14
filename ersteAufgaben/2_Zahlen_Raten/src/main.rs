83b6a835e0f59d069c152ed2b56d094432ac4ccc
OliverWich <kontakt@oliver-wich.de>
2024-05-14 14:40:00 +0200

    implement Aufgabe 2: Bonus 2

--- ersteAufgaben/2_Zahlen_Raten/src/main.rs
+++ ersteAufgaben/2_Zahlen_Raten/src/main.rs
@@ -5,16 +5,49 @@ use std::io;
 const MAX_TRIES: i32 = 6;
 
 fn main () {
+    println!("Willkommen beim Zahlenraten!");
+
+    loop {
+        game_loop();
+
+        if !ask_for_retry() {
+            println!("Auf Wiedersehen!");
+            break;
+        }
+    }
+}
+
+fn ask_for_retry() -> bool {
+    loop {
+        println!("Möchten Sie nochmal spielen? (J/n)");
+
+        let mut play_again = String::new();
+        io::stdin().read_line(&mut play_again).expect("Fehler beim Einlesen der Antwort");
+
+        let raw_command = play_again.trim().to_lowercase();
+
+        let command = if raw_command.is_empty() { "j" } else { &raw_command };
+
+        match command {
+            "j" => return true,
+            "n" => return false,
+            _ => {
+                println!("Ungültige Eingabe! Bitte geben Sie \"j\" oder \"n\" ein.");
+                continue;
+            }
+        }
+    }
+}
+
+fn game_loop() {
     let number = thread_rng().gen_range(1..=100);
     let mut tries = 0;
-    
-    println!("Willkommen beim Zahlenraten!");
-    
+
     loop {
         let mut guess = String::new();
         println!("Bitte geben Sie eine Zahl ein. Sie haben noch {} Versuche:", MAX_TRIES - tries);
         io::stdin().read_line(&mut guess).expect("Fehler beim Einlesen der Zahl");
-        
+
         let guessed_number: i32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => {
@@ -22,7 +55,7 @@ fn main () {
                 continue;
             }
         };
-        
+
         match guessed_number.cmp(&number) {
             Ordering::Less => println!("Die gesuchte Zahl ist größer!"),
             Ordering::Equal => {
@@ -31,13 +64,12 @@ fn main () {
             },
             Ordering::Greater => println!("Die gesuchte Zahl ist kleiner!"),
         }
-        
+
         tries += 1;
-        
+
         if tries >= MAX_TRIES {
             println!("Sie haben keine Versuche mehr! Die gesuchte Zahl war: {}", number);
             break;
         }
     }
-    
 }
